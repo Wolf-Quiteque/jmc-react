537fc31773fa15de8466490fa3c675910891b6e5
Wolf-quiteque <marcioqui3@gmail.com>
2025-08-15 13:49:10 +0100

    kljklj

--- tina/config.js
+++ tina/config.js
@@ -1137,6 +1137,30 @@ export default defineConfig({
           router: () => "/",
         },
       },
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
     ],
   },
 });
